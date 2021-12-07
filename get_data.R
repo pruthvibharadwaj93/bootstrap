436a15de4a5003b5c82b82efb3388190a23f3e61
jyli <jinyuli@bu.edu>
2021-12-07 17:53:48 -0500

    <Updated the get_data file> Now the function can detect what is in the "data" folder and read all csv in it. Next time if someone find more data in the same format, they can just save them into "data" folder and run this function.
    Notion: the format of each csv file should be the same.(same column names)

--- get_data.R
+++ get_data.R
@@ -1,18 +1,30 @@
 library(tidyverse)
 
-
-
-df_tmy3 <- data.frame()
-
-for (year in 1998:2020) {
-  path <- paste0("data","/1340803_42.33_-71.02","_",year,".csv")
-  d <- read.csv(path, header = FALSE)
-  d <- d[c(-1:-2),]
-  colnames(d) <- d[1,]
-  d <- d[-1,]
-  df_tmy3 <- rbind(df_tmy3,d)
+get_data <- function(){
+  # create a data frame to storage the data
+  df_tmy3 <- data.frame()
   
+  # run the for loop to get all data we need
+  for (file1 in list.files(paste0(getwd(),"/data"))) {
+    # Using list.files(paste0(getwd(),"/data")) to get the path of the data
+    # we need for bootstrapping
+    
+    # save the data into dataframe
+    d <- read.csv(paste("data",file1, sep = "/"))
+    # change the column names
+    colnames(d) <- d[2,]
+    
+    # drop the useless rows
+    d <- d[c(-1:-2),]
+    df_tmy3 <- rbind(df_tmy3,d)
+    
+    
+  }
+  # change the row names
+  row.names(df_tmy3) <- c(1:nrow(df_tmy3))
   
+  # output the dataframe that we need for bootstrapping
+  return(df_tmy3)
 }
-
-row.names(df_tmy3) <- c(1:nrow(df_tmy3))
+# demo
+# df1 <- get_data()
